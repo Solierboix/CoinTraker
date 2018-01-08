c0cb0f3b100fc295e20c85b428d003e9aaed62ad
Piotr Walczak <piotr.x.walczak@sony.com>
2018-01-08 17:27:46 +0100

    Add retrofit cleint object to MainActivity

--- app/src/main/java/com/example/walczpio/cointraker/MainActivity.java
+++ app/src/main/java/com/example/walczpio/cointraker/MainActivity.java
@@ -3,11 +3,30 @@ package com.example.walczpio.cointraker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+import com.example.walczpio.cointraker.CoinData.CoinData;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    String BASE_URL = "https://api.coinmarketcap.com/v1/ticker/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).baseUrl(BASE_URL).build();
+        CoinMarketCapAPI client = retrofit.create(CoinMarketCapAPI.class);
+
     }
 }
