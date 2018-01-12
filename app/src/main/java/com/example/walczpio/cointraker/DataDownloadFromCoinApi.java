b019f29d053b642080e78cdece324d9e3431f54f
Piotr Walczak <piotr.x.walczak@sony.com>
2018-01-12 17:20:22 +0100

    Modiffy DbHelper and Json download classes

--- app/src/main/java/com/example/walczpio/cointraker/DataDownloadFromCoinApi.java
+++ app/src/main/java/com/example/walczpio/cointraker/DataDownloadFromCoinApi.java
@@ -1,4 +1,39 @@
 package com.example.walczpio.cointraker;
 
+import com.example.walczpio.cointraker.CoinData.CoinData;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class DataDownloadFromCoinApi {
+
+    private DatabaseHelper mDatabaseHelper = new DatabaseHelper();
+
+
+    public void getDataFromJson(String coin){
+        CoinApiCall apiCall = new CoinApiCall();
+        apiCall.getCoinMarketData(coin).enqueue(new Callback<List<CoinData>>() {
+            @Override
+            public void onResponse(Call<List<CoinData>> call, Response<List<CoinData>> response) {
+                String id = response.body().get(0).getId();
+                String name = response.body().get(0).getName();
+                String symbol = response.body().get(0).getSymbol();
+                String priceUSD = response.body().get(0).getPriceUsd();
+                String percentChange1h = response.body().get(0).getPercentChange1h();
+                String percentChange24h = response.body().get(0).getPercentChange24h();
+                String percentChange7d = response.body().get(0).getPercentChange7d();
+
+                mDatabaseHelper.addResultsToRealmDB(id, name, symbol, priceUSD, percentChange1h, percentChange24h, percentChange7d);
+
+            }
+            @Override
+            public void onFailure(Call<List<CoinData>> call, Throwable t) {
+
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/example/walczpio/cointraker/DatabaseHelper.java
+++ app/src/main/java/com/example/walczpio/cointraker/DatabaseHelper.java
@@ -1,14 +1,15 @@
 package com.example.walczpio.cointraker;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.walczpio.cointraker.model.CryptoCoin;
 
 import io.realm.Realm;
 
-class DatabaseOperations {
+class DatabaseHelper {
 
-    private final static String TAG = "DatabaseOperations";
+    private final static String TAG = "DatabaseHelper";
     private final Realm realm = Realm.getDefaultInstance();
 
     void addResultsToRealmDB(final String id, final String name, final String symbol,
@@ -17,8 +18,7 @@ class DatabaseOperations {
         realm.executeTransactionAsync(new Realm.Transaction() {
             @Override
             public void execute(Realm realm) {
-                CryptoCoin cryptoCoin = realm.createObject(CryptoCoin.class);
-                cryptoCoin.setId(id);
+                CryptoCoin cryptoCoin = realm.createObject(CryptoCoin.class, id);
                 cryptoCoin.setName(name);
                 cryptoCoin.setSymbol(symbol);
                 cryptoCoin.setPriceUSD(priceUSD);
@@ -30,11 +30,13 @@ class DatabaseOperations {
             @Override
             public void onSuccess() {
                 Log.v(TAG, "Data inserted");
+                Toast.makeText(MyApplication.getAppContext(), "Data downloaded", Toast.LENGTH_LONG).show();
             }
         }, new Realm.Transaction.OnError() {
             @Override
             public void onError(Throwable error) {
                 Log.e(TAG, "onError: " + error.getMessage());
+                Toast.makeText(MyApplication.getAppContext(), error.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
